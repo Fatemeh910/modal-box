b7e804e1b6b05e84db1b08df56abb9845e348514
Fatemeh910 <fatemehkhoshnod91@gmail.com>
2023-12-31 15:13:34 +0330

    Openbtn

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-const openButton = document.querySelector(".open-btn")
+///elemets///
+const openButton = document.querySelector(".open-btn");
+const closeButton = document.querySelector(".close-btn");
+const modalBackdrop = document.querySelector(".modal-backdrop");
+
+
+openButton.addEventListener('click', () => {
+    modalBackdrop.classList.add("show-modal");
+});
+
+closeButton.addEventListener('click', () => {
+    modalBackdrop.classList.remove("show-modal");
+});
