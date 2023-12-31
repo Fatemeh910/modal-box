6e350fd834b95fd013dd3ac21b9a1cd38280c960
Fatemeh910 <fatemehkhoshnod91@gmail.com>
2023-12-31 16:31:35 +0330

    End comit

--- main.js
+++ main.js
@@ -3,16 +3,26 @@ const openButton = document.querySelector(".open-btn");
 const closeButton = document.querySelector(".close-btn");
 const modalBackdrop = document.querySelector(".modal-backdrop");
 
-//OPEN MODAL
+//  OPEN MODAL BOX
 openButton.addEventListener("click", () => {
     toggleModal();
 });
 
-//CLoSE MODAL Box
+//  CLoSE MODAL Box
 closeButton.addEventListener("click", () => {
     toggleModal();
 });
 
+
+//  CLoSE MODAL Box WHEN click out of the box content
+modalBackdrop.addEventListener('click', (e)=>{
+const currentElement = e.target;
+
+
+if(currentElement.classList.contains('modal-Backdrop')) {
+    toggleModal();
+}
+});
 
 
 const toggleModal = () => {
