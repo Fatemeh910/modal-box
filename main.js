af18180b184d3b38d88534e62992f6064547c285
Fatemeh910 <fatemehkhoshnod91@gmail.com>
2023-12-31 15:48:53 +0330

    Add main.js

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-///elemets///
+//elemets
 const openButton = document.querySelector(".open-btn");
 const closeButton = document.querySelector(".close-btn");
 const modalBackdrop = document.querySelector(".modal-backdrop");
 
-
-openButton.addEventListener('click', () => {
-    modalBackdrop.classList.add("show-modal");
+//OPEN MODAL
+openButton.addEventListener("click", () => {
+    toggleModal();
 });
 
-closeButton.addEventListener('click', () => {
-    modalBackdrop.classList.remove("show-modal");
+//CLoSE MODAL Box
+closeButton.addEventListener("click", () => {
+    toggleModal();
 });
+
+
+
+const toggleModal = () => {
+    modalBackdrop.classList.toggle("show-modal");
+};
